cb773e2c016a2829e51d6422d89cb9e2f1b60988
RicardoD4W <ricardo.rodriguez@fernando3martos.com>
2022-09-18 22:23:07 +0200

    add

---
+++ main.js
@@ -0,0 +1,41 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b47a50bc33msh5b59a511e550edbp156cf5jsndf53f2174c07',
+		'X-RapidAPI-Host': 'ip-geolocation-ipwhois-io.p.rapidapi.com'
+	}
+};
+
+
+const fetchIPinfo = ip =>{
+    return fetch(`https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/?ip=${ip}`, options)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+}
+
+
+const $ = selector => document.querySelector(selector);
+
+const $form = $('.form');
+const $input = $('.input');
+const $submit = $('.submit');
+const $results = $('.results');
+
+$form.addEventListener('submit', async (e) =>{
+    e.preventDefault();
+    const {value} = $input;
+    if(!value) return;
+
+    $submit.setAttribute('disable', '');
+    $submit.setAttribute('aria-busy', 'true');
+
+    const ipInfo = await fetchIPinfo(value);
+
+    if(ipInfo){
+        $results.innerHTML = JSON.stringify(ipInfo);
+    }
+
+    $submit.removeAttribute('disable');
+    $submit.removeAttribute('aria-busy');
+});
+
