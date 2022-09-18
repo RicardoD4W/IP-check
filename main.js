90ebb870d8e98531c5179a679301d305b5bb9209
RicardoD4W <ricardo.rodriguez@fernando3martos.com>
2022-09-18 22:31:32 +0200

    add

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ $form.addEventListener('submit', async (e) =>{
     const ipInfo = await fetchIPinfo(value);
 
     if(ipInfo){
-        $results.innerHTML = JSON.stringify(ipInfo, null, 2);
+        $results.innerHTML = JSON.stringify(ipInfo, null, 1);
     }
 
     $submit.removeAttribute('disable');
